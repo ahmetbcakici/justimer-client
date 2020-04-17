7cf8ef83342978584d66c2d42b09e11b51f4a9a4
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-17 05:35:19 +0300

    timerData state is empty string in timer function problem solved by Mert

--- src/Timer.js
+++ src/Timer.js
@@ -1,63 +1,3 @@
-
-/*
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import {differenceInSeconds, differenceInMinutes} from 'date-fns';
-
-import axios from 'axios';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import './Timer.css';
-
-import React, {Component} from 'react';
-
-export default class Timer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      timerData: '',
-      isAdmin: '',
-      min: 0,
-      sec: 59,
-      timerlink: props.match.params.timerlink,
-    };
-  }
-
-  async componentDidMount() {
-    const {timerlink} = this.state;
-    const {data} = await axios.get(`/api/timers/${timerlink}`);
-    this.setState({
-      timerData: data,
-      isAdmin: data.adminLink === timerlink,
-      min: data.isPomodoro ? 24 : 0,
-    });
-
-    setInterval(() => {
-      this.timer();
-    }, 1000);
-  }
-
-  timer = () => {
-    console.log('bom');
-    const {runTimerTime} = this.state.timerData;
-    const start = new Date(runTimerTime);
-    console.log(start);
-    const current = new Date();
-    const diffInSec = differenceInSeconds(current, start);
-    const diffInMin = differenceInMinutes(current, start);
-    if (isNaN(diffInMin)) return;
-    const test = diffInSec - diffInMin * 60;
-    console.log(test);
-    console.log(diffInSec, diffInMin);
-    this.setState({min: 24 - diffInMin, sec: 59 - test});
-    //* console.log(diffMin);
-  };
-
-  startTimer = () => {
-    const {adminLink} = this.state.timerData;
-    axios.put('/api/setruntime', {adminLink});
-  };
-}*/
-
 import React, {useState, useEffect} from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import {differenceInSeconds, differenceInMinutes} from 'date-fns';
@@ -66,34 +6,42 @@ import axios from 'axios';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './Timer.css';
+import {startOfYesterday} from 'date-fns/esm';
 
 export default function Timer(props) {
+  //const [timerData, setTimerData] = useState('');
   const [timerData, setTimerData] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
   const [min, setMin] = useState(0);
   const [sec, setSec] = useState(59);
   const {timerlink} = props.match.params;
 
-  useEffect(async () => {
-    const {data} = await axios.get(`/api/timers/${timerlink}`);
-    setIsAdmin(data.adminLink === timerlink)
-    setMin(data.isPomodoro ? 24 : 0);
-    setTimerData(data);
-
-    setInterval(() => {
-      timer(data);
-    },1000)
+  useEffect(() => {
+    getDatasetStates();
   }, []);
 
-  const timer = data => {
-    const {runTimerTime} = data;
+  useEffect(() => {
+    setInterval(() => {
+      if (timerData !== '') timer();
+    }, 1000);
+  }, [timerData]);
+
+  const timer = () => {
+    const {runTimerTime} = timerData;
     const start = new Date(runTimerTime);
     const current = new Date();
     const diffInSec = differenceInSeconds(current, start);
     const diffInMin = differenceInMinutes(current, start);
     const diffInSecRemain = diffInSec - diffInMin * 60;
-    setMin(25 - diffInMin);
+    setMin(24 - diffInMin);
     setSec(59 - diffInSecRemain);
+  };
+
+  const getDatasetStates = async () => {
+    const {data} = await axios.get(`/api/timers/${timerlink}`);
+    setIsAdmin(data.adminLink === timerlink);
+    setMin(data.isPomodoro ? 24 : 0);
+    setTimerData(data);
   };
 
   const startTimer = () => {
@@ -101,11 +49,13 @@ export default function Timer(props) {
     axios.put('/api/setruntime', {adminLink});
   };
 
+  const {viewLink, bellSound} = timerData;
+  const {hostname} = window.location;
   return (
     <div>
       <Navbar />
       {/* Viewer Link Area */}
-      <p className="p-2 bg-dark text-white text-center">
+      <div className="p-2 bg-dark text-white text-center">
         <span className="input-group input-group-center w-25">
           <span className="pt-1">Viewer Link:</span>&nbsp;&nbsp;
           <input
@@ -113,13 +63,11 @@ export default function Timer(props) {
             className="form-control"
             aria-label="Recipient's username"
             aria-describedby="basic-addon2"
-            value={`${window.location.hostname}/${timerData.viewLink}`}
+            value={`${hostname}/${viewLink}`}
           />
           <div className="input-group-append">
             <span className="input-group-text" id="basic-addon2">
-              <CopyToClipboard
-                text={`${window.location.hostname}/${timerData.viewLink}`}
-              >
+              <CopyToClipboard text={`${hostname}/${viewLink}`}>
                 <a href="#">
                   <i className="fas fa-copy"></i>
                 </a>
@@ -134,35 +82,39 @@ export default function Timer(props) {
             </span>
           </div>
         </span>
-      </p>
+      </div>
 
       {/* Timer Area */}
       <div className="text-center">
         <p className="display-1">{`${min}:${sec}`}</p>
         <button
           type="button"
-          class="btn btn-secondary btn-lg w-25"
+          className="btn btn-secondary btn-lg w-25"
           onClick={startTimer}
         >
           START Timer
         </button>
         <br />
         <br />
-        <div class="form-check w-25 input-group-center border py-2">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
+        <div className="form-check w-25 input-group-center border py-2">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
             Repeat timer sequence forever
           </label>
         </div>
         <br />
-        <div class="input-group mb-3 w-25 input-group-center">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1">
-              <i class="fas fa-bell"></i>
+        <div className="input-group mb-3 w-25 input-group-center">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">
+              <i className="fas fa-bell"></i>
             </span>
           </div>
-          <select class="custom-select" id="inputGroupSelect01">
-            <option selected>{timerData.bellSound}</option>
+          <select className="custom-select" id="inputGroupSelect01">
+            <option defaultValue>{bellSound}</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
