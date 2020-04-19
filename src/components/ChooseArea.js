e16db603af6b899ba11a7c2743e92ce36607c9bc
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-19 17:44:40 +0300

    modal installed and its working correctly

--- src/components/ChooseArea.js
+++ src/components/ChooseArea.js
@@ -1,11 +1,29 @@
 import React,{useState} from 'react';
 import axios from 'axios';
+import Modal from 'react-modal';
 import {Redirect} from 'react-router-dom';
 
 import './ChooseArea.css';
 
+Modal.setAppElement('#root')
+
 export default function ChooseArea() {
+  ///var subtitle;
   const [redirectTo, setRedirectTo] = useState('')
+  const [isModalOpen,setIsModalOpen] = useState(false);
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+ 
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    //subtitle.style.color = '#f00';
+  }
+ 
+  function closeModal(){
+    setIsModalOpen(false);
+  }
 
   const generateTimer = async (isPomodoro = false) => {
     if (isPomodoro !== true) isPomodoro = false; 
@@ -23,7 +41,7 @@ export default function ChooseArea() {
         </p>
         <p className="text-center text-white">Choose A Option:</p>
         <div className="row text-center">
-          <div className="col-4">
+          <div className="col-6">
             <button
               className="btn btn-danger"
               onClick={() => generateTimer(true)}
@@ -31,18 +49,44 @@ export default function ChooseArea() {
               POMODORO
             </button>
           </div>
-          <div className="col-4">
-            <button className="btn btn-danger" onClick={generateTimer}>
+          <div className="col-6">
+            <button className="btn btn-danger" onClick={openModal}>
               CUSTOM TIMER
-            </button>
-          </div>
-          <div className="col-4">
-            <button className="btn btn-danger" onClick={generateTimer}>
-              >KITCHEN TIMER
             </button>
           </div>
         </div>
       </div>
+
+        <Modal
+          isOpen={isModalOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customModalStyles}
+          contentLabel="Example Modal"
+        >
+ 
+          <h2>Hello</h2>
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
     </div>
   );
 }
+
+const customModalStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+  }
+};
