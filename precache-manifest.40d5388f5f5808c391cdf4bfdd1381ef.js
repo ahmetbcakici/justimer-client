6e59de2c712d6c8af2877098e9d82fca1e427b01
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-20 22:23:36 +0300

    Updates

---
+++ precache-manifest.40d5388f5f5808c391cdf4bfdd1381ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabe37ce854eb3e8619d5c1479753e65",
+    "url": "/justimer-client/index.html"
+  },
+  {
+    "revision": "34a4fedaef557fe8b14c",
+    "url": "/justimer-client/static/css/main.15dc6f8f.chunk.css"
+  },
+  {
+    "revision": "2c8821a6ebc2f629a478",
+    "url": "/justimer-client/static/js/2.460e2569.chunk.js"
+  },
+  {
+    "revision": "cd10adfcd58d7f62f65d9f074b9081c8",
+    "url": "/justimer-client/static/js/2.460e2569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a4fedaef557fe8b14c",
+    "url": "/justimer-client/static/js/main.d412f7c5.chunk.js"
+  },
+  {
+    "revision": "fcdaf8d885ee05f8e921",
+    "url": "/justimer-client/static/js/runtime-main.48778d15.js"
+  },
+  {
+    "revision": "a9d98db57aa7a8a66c849d8bc941e948",
+    "url": "/justimer-client/static/media/bell.a9d98db5.mp3"
+  }
+]);
