aba6cb102cb8495898fba2debcd34edd7a6f72d6
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-24 00:34:01 +0300

    navbar is ready with github banner at left corner & logo at right corner

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import logo from './logo.png';
+import GithubCorner from 'react-github-corner';
 import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-light">
-      <a className="navbar-brand text-white" href="/">
-        JusTimer
-      </a>
+    <nav className="navbar navbar-expand p-0 m-0">
+      <GithubCorner
+        href="https://github.com/ahmetbcakici/justimer-client"
+        direction={'left'}
+        size={107}
+      />
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto"></ul>
+        <ul>
+          <a href="/">
+          <img src={logo} alt="Logo" className="logo" />
+          </a>
+        </ul>
+      </div>
     </nav>
   );
 }
