453c9359da85eac8e3a811503785dcb4c7e7482d
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-24 02:10:09 +0300

    Footer component design ready to publish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
-    return (
-        <div>
-            <p className="h-4 text-center">MADE BY IBES</p>
-        </div>
-    )
+  return (
+    <footer className="pb-1 pt-3">
+        <p className="text-center">
+      <a
+        className="github-button"
+        href="https://github.com/ntkme/github-buttons"
+        data-icon="octicon-star"
+        data-size="large"
+        aria-label="Star ntkme/github-buttons on GitHub"
+      >
+        Star & Contribute this project on GitHub
+      </a>
+      </p>
+    </footer>
+  );
 }
